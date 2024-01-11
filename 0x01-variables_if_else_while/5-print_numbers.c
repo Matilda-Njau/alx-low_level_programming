18581aa07f7ab500e95868a4b4c5e4a2137c02b3
Matilda-Njau <matildawanjirunjau@gmail.com>
2024-01-11 01:50:25 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	int Base10SingleNum;
+	for (Base10SingleNum='0', Base10SingleNum<='9', Base10SingleNum++)
+		{
+			printf("%d\n", Base10SingleNum);
+		}
+	return(0);
+}
